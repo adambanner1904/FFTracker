68b1035e626e989d971884e482a9f35cb614ca5e
adambanner <adamajbbanner@hotmail.co.uk>
2023-12-26 16:49:45 +0000

    Some work done to set up the inital db that at the moment will only include a player table.

--- website/__init__.py
+++ website/__init__.py
@@ -1,17 +1,26 @@
+from os import path
+
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from website.events.events import events_bp
 from website.people.people import people_bp
 from website.kitty.kitty import kitty_bp
 from website.views import views
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hellostensetheisrntesrn'
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
+
     from .events import events
-    from .kitty import templates
+    from .kitty import kitty
     from .people import people
 
     app.register_blueprint(events_bp, url_prefix='/events')
@@ -19,4 +28,12 @@ def create_app():
     app.register_blueprint(people_bp, url_prefix='/people')
     app.register_blueprint(views)
 
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        db.create_all()
+        print("Created database!")
--- website/people/people.py
+++ website/people/people.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 people_bp = Blueprint('people', __name__, template_folder='templates',
                       static_folder='static', static_url_path='assets')
@@ -9,6 +9,10 @@ def people():
     return render_template('people.html')
 
 
-@people_bp.route('/add-player')
+@people_bp.route('/add-player', methods=['GET', 'POST'])
 def add_player():
+    if request.method == 'POST':
+
+        name = request.form.get('name')
+        print(name)
     return render_template('add_player.html')
