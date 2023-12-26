46b7eae812e0006b1163e8a8376a82f3c785eafa
adambanner <adamajbbanner@hotmail.co.uk>
2023-12-26 22:16:10 +0000

    Started on events page. Added the buttons to go to the add training/game pages, haven't yet built the forms for those yet, that is the next step.

--- website/events/events.py
+++ website/events/events.py
@@ -7,3 +7,13 @@ events_bp = Blueprint('events', __name__, template_folder='templates',
 @events_bp.route('/')
 def events():
     return render_template('events.html')
+
+
+@events_bp.route('/add-training')
+def add_training():
+    return render_template('add_training.html')
+
+
+@events_bp.route('/add-game')
+def add_game():
+    return render_template('add_game.html')
